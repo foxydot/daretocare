cfa3359898721816d8bc873a076d7a6384e63417
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-02-17 21:08:25 -0500

    facebook

--- assets/themes/greatharvest/lib/inc/shortcodes.php
+++ assets/themes/greatharvest/lib/inc/shortcodes.php
@@ -111,3 +111,17 @@ function msdlab_icon_shortcodes($atts){
     }
     return '<i class="'.implode(" ",$classes).'"></i>';
 }
+
+add_shortcode('fb_code','fb_code_function');
+function fb_code_function(){
+    print('
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1385994154976512";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, \'script\', \'facebook-jssdk\'));</script>
+    ');
+}
