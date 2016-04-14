b651c05b2509d7f87062fedb288792ce24814836
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-04-13 22:23:19 -0400

    try to fix up map issues

--- assets/themes/greatharvest/lib/js/map_style_overrides.js
+++ assets/themes/greatharvest/lib/js/map_style_overrides.js
@@ -1,14 +1,23 @@
 jQuery(document).ready(function($) {
+    $('select[name="map_radius"]').val('5').trigger('change');
     var search_html = '<div class="search"><input class="wpgmp_search_input_visible" type="text" placeholder="Enter address, city, or ZIP code" name="wpgmp_search_input" rel="24"><input class="wpgmp_search_input_submit" type="submit" value="submit"></div>';
     var radius_html = '<div class="radius"><select id="map_radius_visible" class="map_radius_visible">' + $('select[name="map_radius"]').html() + '</select></div>';
-    $('.wpgmp_map_container .wpgmp_map').before('<div class="visible_search">' + search_html + radius_html + '</div>');
+    $('.wpgmp_map_container .wpgmp_map').before('<div class="visible_search">' + search_html + '</div>');
     $('.wpgmp_map_container .wpgmp_map .wpgmp_tabs_container .wpgmp_tab_item:first-child .wpgmp_specific_category_item').prop('checked', true).trigger('change');
     $('.wpgmp_search_input_visible').change(function(){
         $('.wpgmp_search_input').val($(this).val());
     });
     $('.wpgmp_search_input_submit').click(function(){
-        $('.wpgmp_search_input').val($('.wpgmp_search_input_visible').val()).trigger('keyup');
-        $('.wpgmp_listing_list .iwlist:first-child .title a').trigger('click');
+        $('.wpgmp_search_input').val($('.wpgmp_search_input_visible').val()).trigger('keyup');    
+        $('.wpgmp_map_container .wpgmp_map .wpgmp_tabs_container .wpgmp_tab_item:first-child .wpgmp_specific_category_item').prop('checked', true).trigger('change');
+        $('select[name="map_radius"]').val('5').trigger('change');
+        var checkExist = setInterval(function() {
+           if ($('.wpgmp_listing_list .iwlist:first-child a.place_title').length) {
+              $('.wpgmp_listing_list .iwlist:first-child a.place_title').trigger('click');
+              clearInterval(checkExist);
+           }
+        }, 100);
+        
     });
     $('#map_radius_visible').change(function(){
         $('select[name="map_radius"]').val($(this).val()).trigger('change');
