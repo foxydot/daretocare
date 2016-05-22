6538fb2668a2d5ca0f4b1846dd12201fb100cf92
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-05-22 11:33:52 -0400

    As a rule for the whole site: DtC client asked that any links to a site outside of the DtC site please open a new window, not leave the site.

--- assets/themes/greatharvest/lib/js/theme-jquery.js
+++ assets/themes/greatharvest/lib/js/theme-jquery.js
@@ -37,4 +37,12 @@ jQuery(document).ready(function($) {
 	    }
 	});
 	
+	
+    $('a').not('[href*="mailto:"]').each(function () {
+        var isInternalLink = new RegExp('/' + window.location.host + '/');
+        if ( ! isInternalLink.test(this.href) ) {
+            $(this).attr('target', '_blank');
+        }
+    });
+	
 });
