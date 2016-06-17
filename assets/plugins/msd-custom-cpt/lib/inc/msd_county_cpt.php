7b73aa6b7ff1fa43f3a1c26ca73a52eb4342d34b
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-06-17 11:15:06 -0400

    Hunger by County Block Home Page:
    The counties across the top of the Hunger by County block need to be spaced out/aired out.
    
    Remove the “Our Mission: Helping Those in Need” line from the Hunger by County block.
    
    Place the icons and portrait in circles. See Hunger by County example.jpg DROPBOX

--- assets/plugins/msd-custom-cpt/lib/inc/msd_county_cpt.php
+++ assets/plugins/msd-custom-cpt/lib/inc/msd_county_cpt.php
@@ -299,6 +299,104 @@ if (!class_exists('MSDCountyCPT')) {
             }
         }
         
+        function swap_state_name_abrv($state,$return = 'short'){
+            $us_state_abbrevs_names = array(
+                'AL'=>'ALABAMA',
+                'AK'=>'ALASKA',
+                'AS'=>'AMERICAN SAMOA',
+                'AZ'=>'ARIZONA',
+                'AR'=>'ARKANSAS',
+                'CA'=>'CALIFORNIA',
+                'CO'=>'COLORADO',
+                'CT'=>'CONNECTICUT',
+                'DE'=>'DELAWARE',
+                'DC'=>'DISTRICT OF COLUMBIA',
+                'FM'=>'FEDERATED STATES OF MICRONESIA',
+                'FL'=>'FLORIDA',
+                'GA'=>'GEORGIA',
+                'GU'=>'GUAM GU',
+                'HI'=>'HAWAII',
+                'ID'=>'IDAHO',
+                'IL'=>'ILLINOIS',
+                'IN'=>'INDIANA',
+                'IA'=>'IOWA',
+                'KS'=>'KANSAS',
+                'KY'=>'KENTUCKY',
+                'LA'=>'LOUISIANA',
+                'ME'=>'MAINE',
+                'MH'=>'MARSHALL ISLANDS',
+                'MD'=>'MARYLAND',
+                'MA'=>'MASSACHUSETTS',
+                'MI'=>'MICHIGAN',
+                'MN'=>'MINNESOTA',
+                'MS'=>'MISSISSIPPI',
+                'MO'=>'MISSOURI',
+                'MT'=>'MONTANA',
+                'NE'=>'NEBRASKA',
+                'NV'=>'NEVADA',
+                'NH'=>'NEW HAMPSHIRE',
+                'NJ'=>'NEW JERSEY',
+                'NM'=>'NEW MEXICO',
+                'NY'=>'NEW YORK',
+                'NC'=>'NORTH CAROLINA',
+                'ND'=>'NORTH DAKOTA',
+                'MP'=>'NORTHERN MARIANA ISLANDS',
+                'OH'=>'OHIO',
+                'OK'=>'OKLAHOMA',
+                'OR'=>'OREGON',
+                'PW'=>'PALAU',
+                'PA'=>'PENNSYLVANIA',
+                'PR'=>'PUERTO RICO',
+                'RI'=>'RHODE ISLAND',
+                'SC'=>'SOUTH CAROLINA',
+                'SD'=>'SOUTH DAKOTA',
+                'TN'=>'TENNESSEE',
+                'TX'=>'TEXAS',
+                'UT'=>'UTAH',
+                'VT'=>'VERMONT',
+                'VI'=>'VIRGIN ISLANDS',
+                'VA'=>'VIRGINIA',
+                'WA'=>'WASHINGTON',
+                'WV'=>'WEST VIRGINIA',
+                'WI'=>'WISCONSIN',
+                'WY'=>'WYOMING',
+                'AE'=>'ARMED FORCES AFRICA \ CANADA \ EUROPE \ MIDDLE EAST',
+                'AA'=>'ARMED FORCES AMERICA (EXCEPT CANADA)',
+                'AP'=>'ARMED FORCES PACIFIC'
+            );
+            if(strlen($state) == 2){
+                if($return == 'short'){
+                    if(array_key_exists(strtoupper($state), $us_state_abbrevs_names)){
+                        $ret = strtoupper($state);
+                    } else {
+                        $ret = false;
+                    }
+                } else {
+                    if(array_key_exists(strtoupper($state), $us_state_abbrevs_names)){
+                        $ret = $us_state_abbrevs_names[strtoupper($state)];
+                    } else {
+                        $ret = false;
+                    }
+                }
+            } else {
+                $us_state_abbrevs_names = array_flip($us_state_abbrevs_names);
+                if($return == 'short'){
+                    if(array_key_exists(strtoupper($state), $us_state_abbrevs_names)){
+                        $ret = $us_state_abbrevs_names[strtoupper($state)];
+                    } else {
+                        $ret = false;
+                    }
+                } else {
+                    if(array_key_exists(strtoupper($state), $us_state_abbrevs_names)){
+                        $ret = $state;
+                    } else {
+                        $ret = false;
+                    }
+                }
+            }
+            return $ret;
+        }
+        
         function county_slider_shortcode_handler(){
             $counties = $this->get_all_counties();
             //the header
@@ -307,7 +405,7 @@ if (!class_exists('MSDCountyCPT')) {
             foreach($counties AS $county){
                 $state = $county->state;
                 $active = $i==0?' active':'';
-                $state_str = $state != $old_state?'<span class="state '.strtolower($state).'">'.$state.': </span>':'';
+                $state_str = $state != $old_state?'<span class="state '.strtolower($state).'">'.$this->swap_state_name_abrv($state).': </span>':'';
                 $title_short = preg_replace('/\sCounty/i','',$county->post_title);
                 $hdr .= '<li class="item-nav'.$active.'" data-target="#counties" data-slide-to="'.$i.'">'.$state_str.'<span class="county">'.$title_short.'</span></li>';
                 $old_state = $state; $i++;
@@ -328,7 +426,6 @@ if (!class_exists('MSDCountyCPT')) {
                 $body .= '
                 <div class="item'.$active.'">
                     <div class="titles">
-                        <h3>Our Mission: Helping Those in Needs</h3>
                         <h2><a href="'.get_post_permalink($county->ID).'">Hunger in '.$county->post_title.'</a></h2>
                     </div>
                     <div class="row">
