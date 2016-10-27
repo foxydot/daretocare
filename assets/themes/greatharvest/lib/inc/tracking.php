ea727b3713934486e0ab70f2ad963e99a20ef70e
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-10-27 10:00:32 -0400

    make tracking cheat

---
+++ assets/themes/greatharvest/lib/inc/tracking.php
@@ -0,0 +1,19 @@
+<?php
+add_action('genesis_before','msdlab_hardcode_gtm');
+
+/*
+ * Bad idea, but faster
+ */
+function msdlab_hardcode_gtm(){
+    if($_SERVER["SERVER_NAME"] == 'daretocare.org'){
+    print "
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQQV4X');</script>
+<!-- End Google Tag Manager -->
+";
+    }
+}
